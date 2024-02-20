ece13ed676f0a5ff46c5d985dc9655934df2213d
Andrea <al22304031@utslrc.edu.mx>
2024-02-20 14:30:22 -0700

    Codigo agregado

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React, { useState , useEffect } from "react";
+import {AiOutlinePlus} from 'react-icons/ai';
+import Todo from './Todo';
+import {db} from './firebase';
+import {query, collection, onSnapshot, updateDoc, doc, addDoc, deleteDoc} from 'firebase/firestore';
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#9bdff9] to-[#c2b0ff]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-orange-400 text-slate-100`,
+  count: `text-center p-2`,
+};
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  //CRUD de la aplicacion
+  //Creacion de todos
+  const createTodo = async (e) =>{
+    e.preventDefault(e);
+    if(input === ''){
+      alert('Por favor eso no es un "to do" valido')
+      return
+    }
+    await addDoc(collection(db, 'todos'), {
+      text: input,
+      completed: false,
+    })
+    setInput('')
+  };
+
+  //Leer todos de la firebase
+  useEffect(()=>{
+    const q = query(collection(db, 'todos'))
+    const unsubscribe = onSnapshot(q, (querySnapshot) =>{
+      let todosArr = []
+      querySnapshot.forEach((doc)=>{
+        todosArr.push({...doc.data(), id: doc.id})
+      });
+      setTodos(todosArr)
+    });
+    return () => unsubscribe()
+  },[]);
+
+  //Actualizar todos
+  const toggleComplete = async (todo) =>{
+    await updateDoc(doc(db, 'todos', todo.id),{
+      completed: !todo.completed
+    });
+  };
+
+  // Eliminar todos
+  const deleteTodo = async (id) =>{
+    await deleteDoc(doc(db, 'todos', id))
+  };
+
+    return (
+      <div className={style.bg}>
+        <div className={style.container}>
+        <h3 className={style.heading}>To do | app</h3>
+        <form onSubmit={createTodo} className={style.form}>
+          <input value={input} 
+          onChange={(e) => setInput(e.target.value)} 
+          className={style.input} 
+          type="text" placeholder="Add To do"/>
+          <button className={style.button}>
+            <AiOutlinePlus size={30} />
+          </button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+            <Todo key={index} todo={todo} toggleComplete={toggleComplete} 
+            deleteTodo={deleteTodo}/>
+          ))}
+        </ul>
+        {todos.length < 1 ? null : (
+        <p className={style.count}>{`You have ${todos.length} todos`}</p> )}
+        </div>
+      </div>
+    );
+  }
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8RPuGTgDz-kaXG_5IiMBdmKCotkYsxME",
+  authDomain: "todo-app-2bc19.firebaseapp.com",
+  projectId: "todo-app-2bc19",
+  storageBucket: "todo-app-2bc19.appspot.com",
+  messagingSenderId: "873490570152",
+  appId: "1:873490570152:web:ec4cfa3fb0058d145f9b9d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
